39e28b66c9358d7d09e15b41698c00dc1423e5fa
wkigen <qiji950@163.com>
2018-04-19 17:54:09 +0800

    update

--- create_patch.bat
+++ create_patch.bat
@@ -1,11 +1,9 @@
 @echo off
-set /p oldApk=请输入旧版本apk路径:
-set /p newApk=请输入新版本apk路径:
-set /p ouputDir=请输入输出目录:
-set /p version=请输入版本号:
+set /p oldApk=input the old apk:
+set /p newApk=input the new apk:
+set /p ouputDir=input the output folder:
+set /p version=input the version:
 
-echo 正在生成补丁...
 java -jar EpimetheusToolLib.jar %oldApk% %newApk% %ouputDir% %version%
-echo 已生成补丁
 
 pause
